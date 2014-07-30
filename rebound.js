e296e638b41340aa6098f4332b202caae7576367
Will Bailey <wbailey@fb.com>
2014-07-30 10:34:17 -0700

    remove dup license info

--- rebound.js
+++ rebound.js
@@ -1,13 +1,3 @@
-/*
- *  Copyright (c) 2014, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
- */
-
 // Rebound
 // =======
 // **Rebound** is a simple library that models Spring dynamics for the
