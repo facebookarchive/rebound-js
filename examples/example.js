1ee568e018a192d51519fed4e232aebc39a98a0f
Will Bailey <wbailey@fb.com>
2014-08-03 09:45:14 -0700

    styling

--- examples/example.js
+++ examples/example.js
@@ -8,11 +8,13 @@ createHamburgerButtonExample = function(container, size, color, bgColor) {
 
 var doit = function() {
   var container = document.getElementById('hamburgerButtonExample');
-  createHamburgerButtonExample(container, 200, '#999');
-  createHamburgerButtonExample(container, 200, '#FFFFFF', '#999');
-  createHamburgerButtonExample(container, 200, '#FF0000');
-  createHamburgerButtonExample(container, 50, '#000');
-  createHamburgerButtonExample(container, 25, '#000');
+  createHamburgerButtonExample(container, 200, '#1194e7');
+  createHamburgerButtonExample(container, 100, '#cc0000');
+  createHamburgerButtonExample(container, 50, '#00cc00');
+  createHamburgerButtonExample(container, 25, '#cc00ff');
+  createHamburgerButtonExample(container, 25, '#00ccff');
+  createHamburgerButtonExample(container, 25, '#ffcc00');
+  createHamburgerButtonExample(container, 25, '#ccff00');
 };
 
 document.addEventListener('DOMContentLoaded', doit);
